9b4595ea82cf43be278266bb71ceecf1840992f3
meghadeopa <megha.deopa13@gmail.com>
2026-06-25 15:24:44 +0530

    Wire S3 sync into pipeline and simplify main to call run_pipeline

--- networksecurity/pipeline/training_pipeline.py
+++ networksecurity/pipeline/training_pipeline.py
@@ -4,6 +4,9 @@ import sys
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
+from networksecurity.constants.training_pipeline import TRAINING_BUCKET_NAME
+from networksecurity.cloud.s3_syncer import S3Sync
+
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
@@ -28,10 +31,10 @@ from networksecurity.entity.artifact_entity import (
     ModelTrainerArtifact,
 )
 
-
 class TrainingPipeline:
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
+        self.s3_sync = S3Sync()
 
     def start_data_ingestion(self):
         try:
@@ -105,8 +108,23 @@ class TrainingPipeline:
                 model_trainer_artifact=model_trainer_artifact,
             )
 
-            logging.info("S3 sync skipped - AWS not configured")
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
 
             return model_evaluation_artifact
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder=self.training_pipeline_config.artifact_dir, aws_bucket_url=aws_bucket_url)
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(folder="final_model", aws_bucket_url=aws_bucket_url)
         except Exception as e:
             raise NetworkSecurityException(e, sys)
